058ca0357da8da331fa837228686efc8f1244075
Dide Aydogan <dide1992@gmail.com>
2023-05-23 11:42:48 -0400

    installed axios and backend data fetched

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-
+import axios from 'axios';
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [tweet, setTweet] = React.useState([]);
+
+  React.useEffect(()=> {
+    const fetchTweet = async ()=> {
+      const res = await axios.get('/tweets')
+      console.log(res)
+    }
+    fetchTweet()
+  },[])
   return (
     <div className="App">
       <h1>Home</h1>
