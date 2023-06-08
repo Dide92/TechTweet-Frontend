1933c8580c20591c21f9b69b7b4923238e54f874
Dide Aydogan <dide1992@gmail.com>
2023-06-08 10:39:00 -0400

    auth

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,43 +1,43 @@
-// import React, { useRef, useContext } from 'react';
-// import { Link } from 'react-router-dom';
-// import { Context } from "./../context/Context";
-// import axios from 'axios';
+import React, { useRef, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Context } from "./../context/Context";
+import axios from 'axios';
 
 
-// function Login() {
-//     const userRef = useRef()
-//     const passwordRef = useRef();
+function Login() {
+    const userRef = useRef()
+    const passwordRef = useRef();
 
-//     const { user, dispatch, isFetching } = useContext(Context)
+    const { user, dispatch, isFetching } = useContext(Context)
 
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         dispatch({ type:"LOGIN_START" });
-//         try{
-//             const res = await axios.post("/auth/login", {
-//                 username: userRef.current.value,
-//                 password: passwordRef.current.value,
-//             });
-//             dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-//         } catch(err) {
-//             dispatch({ type: "LOGIN_FAILUIRE" })
-//         }
-//     }
-//     console.log(user)
-//     return(
-//     <div className='login'>
-//         <span className='loginTitle'>Login Title</span>
-//         <form className='loginForm' onSubmit={handleSubmit}>
-//             <label>Username</label>
-//             <input type="text" placeholder='Enter your username'  ref={userRef}/>
-//             <label>Password</label>
-//             <input type="password" placeholder='Enter your password' ref={passwordRef} />
-//             <button className='loginButton' type="submit" disabled={isFetching}>Login</button>
-//         </form>
-//         <button className='registerButton'><Link to="/register">Register</Link></button>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch({ type:"LOGIN_START" });
+        try{
+            const res = await axios.post("/auth/login", {
+                username: userRef.current.value,
+                password: passwordRef.current.value,
+            });
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        } catch(err) {
+            dispatch({ type: "LOGIN_FAILUIRE" })
+        }
+    }
+    console.log(user)
+    return(
+    <div className='login'>
+        <span className='loginTitle'>Login Title</span>
+        <form className='loginForm' onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input type="text" placeholder='Enter your username'  ref={userRef}/>
+            <label>Password</label>
+            <input type="password" placeholder='Enter your password' ref={passwordRef} />
+            <button className='loginButton' type="submit" disabled={isFetching}>Login</button>
+        </form>
+        <button className='registerButton'><Link to="/register">Register</Link></button>
 
-//     </div>
-//     )
-// }
+    </div>
+    )
+}
 
-// export default Login;
+export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,44 +1,44 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
-// function Register() {
-//     const [username, setUsername] = React.useState("")
-//     const [email, setEmail] = React.useState("")
-//     const [password, setPassword] = React.useState("")
-//     const [error, setError] = React.useState(false)
+function Register() {
+    const [username, setUsername] = React.useState("")
+    const [email, setEmail] = React.useState("")
+    const [password, setPassword] = React.useState("")
+    const [error, setError] = React.useState(false)
 
-//     const handleSubmit = async (e) => {
-//         e.preventDefault()
-//         setError(false);
-//         try{
-//             const res = await axios.post("/auth/register", {
-//                 username,
-//                 email,
-//                 password,
-//             });
-//             res.data && window.location.replace("/login")
-//         }catch(err){
-//             setError(true)
-//     }   
-//     };
-//     return(
-//     <div className='login'>
-//         <span className='registerTitle'>Register</span>
-//         <form className='registerForm' onSubmit={handleSubmit}>
-//             <label>username</label>
-//             <input type="text" placeholder='Enter your username' onChange={(e=>setUsername(e.target.value))}/>
-//             <label>Email</label>
-//             <input type="text" placeholder='Enter your email' onChange={(e=>setEmail(e.target.value))}/>
-//             <label>Password</label>
-//             <input type="password" placeholder='Enter your password' onChange={(e=>setPassword(e.target.value))}/>
-//             <button className='registerButton' type="submit" >Register</button>
-//         </form>
-//         <button className='loginButton'><Link to="/login">Login</Link></button>
-//         {error && <span>User exists, please login</span>}
-//     </div>
-//     )
-// }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError(false);
+        try{
+            const res = await axios.post("/auth/register", {
+                username,
+                email,
+                password,
+            });
+            res.data && window.location.replace("/login")
+        }catch(err){
+            setError(true)
+    }   
+    };
+    return(
+    <div className='login'>
+        <span className='registerTitle'>Register</span>
+        <form className='registerForm' onSubmit={handleSubmit}>
+            <label>username</label>
+            <input type="text" placeholder='Enter your username' onChange={(e=>setUsername(e.target.value))}/>
+            <label>Email</label>
+            <input type="text" placeholder='Enter your email' onChange={(e=>setEmail(e.target.value))}/>
+            <label>Password</label>
+            <input type="password" placeholder='Enter your password' onChange={(e=>setPassword(e.target.value))}/>
+            <button className='registerButton' type="submit" >Register</button>
+        </form>
+        <button className='loginButton'><Link to="/login">Login</Link></button>
+        {error && <span>User exists, please login</span>}
+    </div>
+    )
+}
 
-// export default Register;
+export default Register;
