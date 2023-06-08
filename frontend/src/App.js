ad93ab48f099b3bd22138c19bfd0b9e93ad6b984
Dide Aydogan <dide1992@gmail.com>
2023-06-08 12:16:58 -0400

    heroku error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -82,12 +82,12 @@ React.useEffect(() => {
           <Route exact path="/events" element={<Events />} />
           <Route exact path="/tweet/:id" element={<TweetDetail tweets={tweets} handleDelete={deleteTweet}/>} />
           <Route exact path="/tweets/:id/edit" element={<EditForm tweets={tweets} handleUpdate={updateTweet} />}/>    
-          {/* <Route path="/register" element= {<Register />} />   
+          <Route path="/register" element= {<Register />} />   
           <Route path="/login" element= {<Login />} /> 
-          <Route path="/setting" element= {<Setting />} />  */}
-          <Route exact path="/setting" element={user ? <Setting /> : <Register />} />
+          <Route path="/setting" element= {<Setting />} /> 
+          {/* <Route exact path="/setting" element={user ? <Setting /> : <Register />} />
           <Route path="/login" element= {user ? <Home/> :<Login />} />
-          <Route path="/register" element= {user ? <Home/> :<Register />} />
+          <Route path="/register" element= {user ? <Home/> :<Register />} /> */}
 
 
       </Routes>
